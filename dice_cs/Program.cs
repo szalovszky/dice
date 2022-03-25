f5c673813490f296169aa8f4e8d5a67efe671937
szalovszky <29980128+szalovszky@users.noreply.github.com>
2022-03-25 13:12:27 +0100

    added csharp

---
+++ dice_cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dobokocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Count: ");
+            int count = int.Parse(Console.ReadLine());
+            Console.Write("Sides: ");
+            int sides = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int sum = 0;
+
+            for (int i = 0; i < count; i++) {
+                int rand = rnd.Next(1, sides - 1);
+                Console.WriteLine(rand.ToString());
+                sum += rand;
+            }
+
+            Console.WriteLine("Sum: " + sum.ToString());
+        }
+    }
+}
